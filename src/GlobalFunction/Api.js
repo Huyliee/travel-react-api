513108f77dbf43ff511cd4bd89f0f17fa3709e6f
trong hieu <hieu745233@gmail.com>
2023-05-04 19:50:44 +0700

    Update https

--- src/GlobalFunction/Api.js
+++ src/GlobalFunction/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const getTour = async ()=>{
-    const res = await axios.get('http://lav2.cf/api/tour');
+    const res = await axios.get('https://lav2.cf/api/tour');
     return res.data.data;
 }
