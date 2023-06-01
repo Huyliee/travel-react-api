de3b46d0b1380f1979f46c1bfce8b5cada006de8
trong hieu <hieu745233@gmail.com>
2023-06-01 20:40:22 +0700

    Time-line and API Weather

--- src/pages/DetailTour/index.js
+++ src/pages/DetailTour/index.js
@@ -9,14 +9,39 @@ import {
   faShoppingCart,
   faTicket,
 } from "@fortawesome/free-solid-svg-icons";
-import { Button, Container, ImageListItem, ImageList ,Box } from "@mui/material";
+import {
+  Button,
+  Container,
+  ImageListItem,
+  ImageList,
+  Box,
+} from "@mui/material";
 import Itinerary from "./Itinerary";
+import axios from "axios";
 
 const cx = classNames.bind(styles);
 
 function DetailTour() {
   const { id } = useParams();
   const [detailTour, setDetailTour] = useState({});
+  const [weather, setWeather] = useState({});
+  const API_KEY_WEATHER = "cd80aaec45724113a6c125437230106";
+  //Api thời tiết
+  useEffect(() => {
+    async function getWeather() {
+      try {
+        const response = await axios.get(
+          `http://api.weatherapi.com/v1/current.json?key=${API_KEY_WEATHER}&q=Da Lat`
+        );
+        setWeather(response?.data);
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    }
+    getWeather();
+  }, []);
+  console.log(weather);
+  //Load api của chi tiết tour
   useEffect(() => {
     async function detailData() {
       const data = await detailTourApi(id);
@@ -24,6 +49,7 @@ function DetailTour() {
     }
     detailData();
   }, [id]);
+  //List Image của MUI
   function modifyImages(images) {
     return images.map((image, index) => {
       if (index === 0) {
@@ -42,6 +68,10 @@ function DetailTour() {
       return image;
     });
   }
+  //Chuyển lên đầu trang khi bấm vào 1 sản phẩm
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className={cx("detail-container")}>
@@ -68,6 +98,16 @@ function DetailTour() {
                 />
                 <label>126</label>
               </div>
+              {/* Hiển thị api thời tiết */}
+              <div className={cx("weather-container")}>
+                <img src={weather?.current?.condition?.icon} alt="Weather" />
+                <p>
+                  {" "}
+                  {weather?.location?.name} - {weather?.current?.temp_c} ° C -{" "}
+                  {weather?.current?.temp_f} ° F
+                </p>
+              </div>
+              {/* /-----------------------------/ */}
             </div>
           </div>
           <div className={cx("detail-infomation-col-2")}>
@@ -96,6 +136,7 @@ function DetailTour() {
           </div>
         </div>
       </Container>
+      {/* Hiển thị List Image của MUI */}
       <Container maxWidth="xl" style={{ padding: "20px 68px" }}>
         <ImageList cols={4} rowHeight={300} style={{ overflow: "hidden" }}>
           {detailTour.images &&
@@ -116,18 +157,21 @@ function DetailTour() {
             ))}
         </ImageList>
       </Container>
+      {/* /---------------------------/ */}
       <Container maxWidth="xl" style={{ padding: "20px 68px" }}>
         <h2 className={cx("content-tour-heading")}>Điểm nhấn</h2>
         <Box>
-            <p className={cx("content-tour-text")}>{detailTour.content_tour}</p>
+          <p className={cx("content-tour-text")}>{detailTour.content_tour}</p>
         </Box>
       </Container>
+      {/* Lịch trình */}
       <Container maxWidth="xl" style={{ padding: "20px 68px" }}>
-      <h2 className={cx("content-tour-heading")}>Lịch trình</h2>
-      <Box>
+        <h2 className={cx("content-tour-heading")}>Lịch trình</h2>
+        <Box>
           <Itinerary />
-      </Box>
+        </Box>
       </Container>
+      {/* /---------------------/ */}
     </div>
   );
 }
